7b1373be7131ba0a78044e09734450f6904c48b7
Tanveer Rajpal <56073437+tanveersrajpal@users.noreply.github.com>
2020-02-14 17:37:04 +1300

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Techofab_Website.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Techofab
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult Aboutus()
+        {
+            return View();
+        }
+
+        public ActionResult Courses()
+        {
+            return View();
+        }
+        
+    }
+}
---
+++ Controllers/PortfolioController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Techofab_Website.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        // GET: Portfolio
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Portfolio1()
+        {
+            return View();
+        }
+        public ActionResult Portfolio2()
+        {
+            return View();
+        }
+        public ActionResult Portfolio3()
+        {
+            return View();
+        }
+    }
+}
